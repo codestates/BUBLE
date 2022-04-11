0d5935fce7bc1205909bf03a1cb54fcf26b55fa4
ghooman <ghoopark1@gmail.com>
2022-04-12 00:43:55 +0900

    login, signup frame

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div>
       <Header />
+      <Login />
       <Footer />
     </div>
   );
